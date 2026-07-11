0fb93b48687cf6e8fc94cf8d7b58a7a1e04f09d0
Jose Flavio <flaviopereiralc@gmail.com>
2026-07-10 22:24:17 -0300

    feat: commit inicial

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import 'dotenv/config'
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import helmet from '@fastify/helmet'
+import { timeStamp } from 'node:console'
+
+
+const PORT = parseInt(process.env.PORT ?? '3000')
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.register(cors, {
+  origin: true,
+  credentials: true,
+})
+
+fastify.register(helmet, {
+  contentSecurityPolicy: false,
+})
+
+// Declare a route
+fastify.get('/', async (request, reply) =>{
+  return {
+    message: 'E-commerceSyntax Wear API',
+    version: '1.0.0',
+    status: 'running',
+
+  }
+})
+
+fastify.get('/health', async (request, reply) => {
+  return {
+    status: 'ok',
+    timeStamp: new Date().toISOString(),
+  }
+})
+
+
+// Run the server!
+fastify.listen({ port: PORT }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
+
+export default fastify
